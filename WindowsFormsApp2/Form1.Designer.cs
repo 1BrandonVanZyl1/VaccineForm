567b9f18aa2e048a85e6b32c6b39f38dde58ec71
1BrandonVanZyl1 <89069026+1BrandonVanZyl1@users.noreply.github.com>
2021-08-25 08:30:07 +0200

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Vaccine
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.idBox = new System.Windows.Forms.TextBox();
+            this.SNameBox = new System.Windows.Forms.TextBox();
+            this.FNameBox = new System.Windows.Forms.TextBox();
+            this.savebtn = new System.Windows.Forms.Button();
+            this.clearbtn = new System.Windows.Forms.Button();
+            this.GenderBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Surname:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(87, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "First Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(87, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Gender:";
+            // 
+            // idBox
+            // 
+            this.idBox.Location = new System.Drawing.Point(199, 64);
+            this.idBox.Name = "idBox";
+            this.idBox.Size = new System.Drawing.Size(182, 22);
+            this.idBox.TabIndex = 4;
+            // 
+            // SNameBox
+            // 
+            this.SNameBox.Location = new System.Drawing.Point(199, 105);
+            this.SNameBox.Name = "SNameBox";
+            this.SNameBox.Size = new System.Drawing.Size(182, 22);
+            this.SNameBox.TabIndex = 5;
+            // 
+            // FNameBox
+            // 
+            this.FNameBox.Location = new System.Drawing.Point(199, 143);
+            this.FNameBox.Name = "FNameBox";
+            this.FNameBox.Size = new System.Drawing.Size(182, 22);
+            this.FNameBox.TabIndex = 6;
+            // 
+            // savebtn
+            // 
+            this.savebtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.savebtn.Location = new System.Drawing.Point(199, 220);
+            this.savebtn.Name = "savebtn";
+            this.savebtn.Size = new System.Drawing.Size(71, 33);
+            this.savebtn.TabIndex = 8;
+            this.savebtn.Text = "SAVE";
+            this.savebtn.UseVisualStyleBackColor = false;
+            this.savebtn.Click += new System.EventHandler(this.savebtn_Click);
+            // 
+            // clearbtn
+            // 
+            this.clearbtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.clearbtn.Location = new System.Drawing.Point(306, 219);
+            this.clearbtn.Name = "clearbtn";
+            this.clearbtn.Size = new System.Drawing.Size(75, 34);
+            this.clearbtn.TabIndex = 9;
+            this.clearbtn.Text = "CLEAR\r\n";
+            this.clearbtn.UseVisualStyleBackColor = false;
+            this.clearbtn.Click += new System.EventHandler(this.clearbtn_Click);
+            // 
+            // GenderBox
+            // 
+            this.GenderBox.Location = new System.Drawing.Point(199, 174);
+            this.GenderBox.Name = "GenderBox";
+            this.GenderBox.Size = new System.Drawing.Size(182, 22);
+            this.GenderBox.TabIndex = 10;
+            // 
+            // Vaccine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GenderBox);
+            this.Controls.Add(this.clearbtn);
+            this.Controls.Add(this.savebtn);
+            this.Controls.Add(this.FNameBox);
+            this.Controls.Add(this.SNameBox);
+            this.Controls.Add(this.idBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Vaccine";
+            this.Text = "Vaccine";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox idBox;
+        private System.Windows.Forms.TextBox SNameBox;
+        private System.Windows.Forms.TextBox FNameBox;
+        private System.Windows.Forms.Button savebtn;
+        private System.Windows.Forms.Button clearbtn;
+        private System.Windows.Forms.TextBox GenderBox;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp2
+{
+    public partial class Vaccine : Form
+    {
+        public Vaccine()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void savebtn_Click(object sender, EventArgs e)
+        {
+            try
+
+            {
+
+                String str = "Data Source=LAPTOP-Q1L5OKJ1;Initial Catalog=PracticalDays;Integrated Security=True";
+
+                String query = "Insert into Users (ID, Surname, FirstName, Gender) VALUES ( @id, @surname, @firstName, @gender)"; ;
+
+                SqlConnection con = new SqlConnection(str);
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                con.Open();
+                cmd.Parameters.AddWithValue("@id", idBox.Text);
+                cmd.Parameters.AddWithValue("@surname", SNameBox.Text);
+                cmd.Parameters.AddWithValue("firstName", FNameBox.Text);
+                cmd.Parameters.AddWithValue("gender", GenderBox.Text);
+
+                
+
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record is saved", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+                DataSet ds = new DataSet();
+
+                
+
+              
+
+                con.Close();
+
+            }
+
+            catch (Exception es)
+
+            {
+
+                MessageBox.Show(es.Message);
+
+
+
+            }
+
+
+
+
+        }
+
+        private void clearbtn_Click(object sender, EventArgs e)
+        {
+            idBox.Clear();
+            SNameBox.Clear();
+            FNameBox.Clear();
+            GenderBox.Clear();
+        }
+
+     
+    }
+}
